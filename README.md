a1adeb37aef7d2acdba8322d4cb6925dd66efa54
Manjeet Singh Bhatia <manjeet.s.bhatia@intel.com>
2017-02-14 07:50:31 +0000

    Add dash script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # neutron_stuff
-for file sharing
+
+Create url for gerrit dashboard for neutron gate failures.
+
+1. Make sure libs launchpad and gerrit-dash-creator are installed
+2. run python create_gate_failure_dash.py 
---
+++ create_gate_failure_dash.py
@@ -0,0 +1,84 @@
+import git
+import os
+
+from subprocess import call
+from gerrit_dash_creator.cmd import creator
+from launchpadlib.launchpad import Launchpad
+
+file_name='gate.dash'
+cachedir = "~/.launchpadlib/cache/"
+launchpad = Launchpad.login_anonymously('just testing', 'production', cachedir,
+                                        version="devel")
+neutron = launchpad.projects['neutron']
+
+def write_section(filen, section_name, query):
+    print(section_name)
+    if query:
+        filen.write("[section \"")
+        filen.write(section_name)
+        filen.write("\"]\n")
+        filen.write("query = ")
+        filen.write(query)
+        print(query)
+    else:
+        print("No result found\n")
+
+
+def _search_task(project, **kwargs):
+    bugs = project.searchTasks(**kwargs)
+    if not bugs:
+        return
+    gerrit_query1 = "("
+    for b in bugs:
+        #gerrit_query1 += ("message:%d OR " % b.bug.id)
+        gerrit_query1 += ("topic:bug/%d OR " % b.bug.id)
+    gerrit_query1 = gerrit_query1[:-4]
+    #gerrit_query2 = gerrit_query2[:-4]
+    gerrit_query1 += ")\n\n"
+    return gerrit_query1
+
+
+def get_gate_failure_query(project, **kwargs):
+    cbg = _search_task(project, **kwargs)
+    return cbg
+
+
+def write_queries_for_project(file_name, project):
+    critical_bugs = {'tags': ['gate-failure'], 'importance': ["Critical"],
+                   'status': ["In Progress"]}
+    High_bugs = {'tags': ['gate-failure'], 'importance': ["High"]}
+
+    ml_bugs = {'tags': ['gate-failure'], 'importance': ["Medium", 'Low']}
+    
+    cb = get_gate_failure_query(project, **critical_bugs)
+    section_name = "Critical Gate failures %s" % project.name
+    write_section(file_name, section_name, cb)
+
+    hg = get_gate_failure_query(project, **High_bugs)
+    
+    section_name = "High priority Gate failures %s" % project.name
+    write_section(file_name, section_name, hg)
+    
+    rest_b = get_gate_failure_query(project, **ml_bugs)
+    section_name = "All other Gate failures %s" % project.name
+    write_section(file_name, section_name, rest_b)
+
+with open(file_name, 'w') as f:
+    title = "[dashboard]\ntitle = Neutron Review Gate Failures\n"
+    f.write(title)
+    f.write("description = Review Inbox\n")
+    f.write("foreach = (project:openstack/neutron OR "
+            "project:openstack/python-neutronclient OR "
+            "project:openstack/neutron-specs OR "
+            "project:openstack/neutron-lib) status:open NOT owner:self "
+            "NOT label:Workflow<=-1 "
+            "NOT label:Code-Review>=-2,self branch:master\n")
+    f.write("\n")
+
+    print("Querying Launchpad, this might take a while...")
+    write_queries_for_project(f, neutron)
+
+import pdb
+if not os.path.exists('gerrit-dash-creator'):  
+    git.Git().clone("https://github.com/openstack/gerrit-dash-creator")
+call('./gerrit-dash-creator/gerrit-dash-creator ' + file_name, shell=True)
